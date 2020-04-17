4ea140017944bc71befa29d32514856c772f1728
BC-Li <251156266@qq.com>
2020-04-17 22:18:15 +0800

    Try to link github and vscode

--- practice/Hanoi.c
+++ practice/Hanoi.c
@@ -1,4 +1,16 @@
 #include <stdio.h>
+void Hanoi(int n,char a,char b,char c){
+    if(n == 1){
+        printf("%c -> %c\n",a,b);
+        return;
+    }
+    Hanoi(n-1,a,c,b);
+    printf("%c -> %c\n",a,b);
+    Hanoi(n-1,c,b,a);
+}
 int main(){
-    
+    int n;
+    scanf("%d",&n);
+    Hanoi(n,'A','B','C');
+    return 0;
 }
