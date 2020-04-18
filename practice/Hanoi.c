dc6acbabf8cadf62b159d93512b2d7b2978c220f
BC-Li <ll>
2020-04-18 09:26:55 +0800

    Hanoi and launch.json added

--- practice/Hanoi.c
+++ practice/Hanoi.c
@@ -3,14 +3,14 @@ void Hanoi(int n,char a,char b,char c){
     if(n == 1){
         printf("%c -> %c\n",a,b);
         return;
-    }
-    Hanoi(n-1,a,c,b);
-    printf("%c -> %c\n",a,b);
-    Hanoi(n-1,c,b,a);
+    }//下面都是前一步的操作，即：n不等于1的时候先做上一步的操作
+    Hanoi(n-1,a,c,b);//那么，前一步也就是f(n - 1, other variables)
+    printf("%c -> %c\n",a,b);//显然是先将n -1 个在A柱子上的盘子通过C柱移动到B柱上，
+    Hanoi(n-1,c,b,a);//再将在A柱子上的编号为n的盘子移动到C柱上，再将B柱子上的n-1个盘子通过A柱移动到C柱上
 }
 int main(){
     int n;
-    scanf("%d",&n);;
+    scanf("%d",&n);
     Hanoi(n,'A','B','C');
     return 0;
 }
