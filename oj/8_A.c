a242a6d27feba029a03e8bbeada1d37e04bed526
BC-Li <251156266@qq.com>
2020-04-22 15:49:14 +0800

    fixed a stupid mistake

--- oj/8_A.c
+++ oj/8_A.c
@@ -1,1 +1,18 @@
-#include <s
+#include <stdio.h>
+int main(){
+    int n,i,j,k,mm=0,max=0;
+    scanf("%d", &n);
+    for ( i = 0; i <= n;i++){
+        for ( j = 0; j <= n;j++){
+            for ( k = 0; k <= n;k++){
+                if((((i+j)%2)==0)&&(((j+k)%3)==0)&&(((i+j+k)%5)==0)){
+                    if(max<i+k+j){
+                        max = i+k+j;
+                    }
+                }
+            }
+        }
+    }
+    printf("%d", max);
+    return 0;
+}
